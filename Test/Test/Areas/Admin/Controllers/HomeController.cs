173fd5d769c618fde5b45ca03de9a011670ef25a
Ducvuive2 <82598608+Ducvuive2@users.noreply.github.com>
2021-12-12 18:11:35 +0700

    Update UI
    Update UI

---
+++ Test/Test/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_CuaHangLaptop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ChiTietSanPham()
+        {
+            return View();
+        }
+        public IActionResult CongKetNoi()
+        {
+            return View();
+        }
+        public IActionResult CPU()
+        {
+            return View();
+        }
+        public IActionResult DanhMuc()
+        {
+            return View();
+        }
+        public IActionResult ManHinh()
+        {
+            return View();
+        }
+        public IActionResult RAM()
+        {
+            return View();
+        }
+    }
+
+}

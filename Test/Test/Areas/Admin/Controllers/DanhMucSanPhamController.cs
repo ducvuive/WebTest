3155e57ce6e8e6c59068a0550d4a05ef9b8b6dd8
honghai19521467 <19521467@gm.uit.edu.vn>
2021-12-23 20:46:44 +0700

    update_slider_deleteProduct
    update_slider_deleteProduct

--- Test/Test/Areas/Admin/Controllers/DanhMucSanPhamController.cs
+++ Test/Test/Areas/Admin/Controllers/DanhMucSanPhamController.cs
@@ -146,12 +146,22 @@ namespace Test.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(string Madm)
         {
+            try
+            {
+
             var danhmucsanpham = await _context.Danhmucsanpham.FindAsync(Madm);
             _context.Danhmucsanpham.Remove(danhmucsanpham);
             await _context.SaveChangesAsync();
             TempData["AlertMessage"] = "Xóa thành công";
             TempData["AlertType"] = "alert alert-success";
             return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                TempData["AlertMessage"] = "Xóa không thành công";
+                TempData["AlertType"] = "alert alert-danger";
+                return RedirectToAction(nameof(Index));
+            }
         }
 
         private bool DanhmucsanphamExists(string id)
--- Test/Test/Areas/Admin/Controllers/ManHinhController.cs
+++ Test/Test/Areas/Admin/Controllers/ManHinhController.cs
@@ -146,12 +146,22 @@ namespace Test.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(string Mamh)
         {
-            var manhinh = await _context.Manhinh.FindAsync(Mamh);
-            _context.Manhinh.Remove(manhinh);
-            await _context.SaveChangesAsync();
-            TempData["AlertMessage"] = "Xóa thành công";
-            TempData["AlertType"] = "alert alert-success";
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                var manhinh = await _context.Manhinh.FindAsync(Mamh);
+                _context.Manhinh.Remove(manhinh);
+                await _context.SaveChangesAsync();
+                TempData["AlertMessage"] = "Xóa thành công";
+                TempData["AlertType"] = "alert alert-success";
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch
+            {
+                TempData["AlertMessage"] = "Xóa không thành công";
+                TempData["AlertType"] = "alert alert-danger";
+                return RedirectToAction(nameof(Index));
+            }
         }
 
         private bool ManhinhExists(string id)
